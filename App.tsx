ba9a9875683b805c76b0b8a7adb7c3531f1d4237
jairordtgz <jairodri.a05@gmail.com>
2025-11-06 18:13:01 -0500

    configuración de tailwind

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+// import { ScreenContent } from 'components/ScreenContent';
+// import { StatusBar } from 'expo-status-bar';
 
-import './global.css';
+// import './global.css';
 
+// export default function App() {
+//   return (
+//     <>
+//       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+//       <StatusBar style="auto" />
+//     </>
+//   );
+// }
+
+import "./global.css"
+import { Text, View } from "react-native";
+ 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500 bg-black">
+        Welcome to Nativewind!
+      </Text>
+    </View>
   );
 }
