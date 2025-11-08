edf19a73402a79a2ab1604cd7f684c145c3456c0
Angel Eduardo Pilataxi Rodriguez <angel.pilataxi123464@gmail.com>
2025-11-08 03:51:59 +0000

    Se agrego firebase

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-
+import app from './utils/firebase';
+import { StyleSheet } from 'react-native';
 import './global.css';
 
 export default function App() {
+
+
   return (
     <>
       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
