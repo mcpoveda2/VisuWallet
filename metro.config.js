94b44d8dd3e2a5ecdf9cf70e4758e0ce7744ee74
Sebastian Barco Gaspar <sgasparbarco@gmail.com>
2025-11-08 19:57:00 -0500

    correcion del metro.config

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,12 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// metro.config.js (VERSIÓN CORRECTA)
 
-/** @type {import('expo/metro-config').MetroConfig} */
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
 const config = getDefaultConfig(__dirname);
+
+// Agregar soporte para archivos .cjs
 config.resolver.sourceExts.push('cjs');
-module.exports = config;
+
+// IMPORTANTE: Aplicar configuración de NativeWind
+module.exports = withNativeWind(config, { input: './global.css' });
